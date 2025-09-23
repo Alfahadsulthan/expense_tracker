6e8a43719113e704d61110a2f1ecdbd3f884a179
alfahad <alfahadsulthan88icloud.com@alfahads-MacBook-Air.local>
2025-09-23 23:33:14 +0530

    added over all total to the month card for reference and mordern ui added

--- lib/screens/month.dart
+++ lib/screens/month.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/models/expense.dart';
 import 'package:expense_tracker/screens/month_group.dart';
 import 'package:expense_tracker/screens/categories.dart';
 import 'package:flutter/material.dart';
@@ -11,10 +12,14 @@ class MonthScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final monthsBox = Hive.box<String>(kBoxMonths);
+    final expenseList = Hive.box<Expense>(kBoxExpenses);
 
     return Scaffold(
+      backgroundColor: Colors.grey[100],
       appBar: AppBar(
-        title: const Text('Months'),
+        title: const Text('Months Overview'),
+        elevation: 0,
+        backgroundColor: Colors.teal,
         actions: [
           IconButton(
             tooltip: 'Add current month',
@@ -26,55 +31,152 @@ class MonthScreen extends StatelessWidget {
           IconButton(
             tooltip: 'Settings',
             icon: const Icon(Icons.settings),
-            onPressed: () =>Navigator.push(
-                context,
-                MaterialPageRoute(builder: (_) => const CategoriesScreen()),
-              ),
+            onPressed: () => Navigator.push(
+              context,
+              MaterialPageRoute(builder: (_) => const CategoriesScreen()),
+            ),
           ),
         ],
       ),
       body: ValueListenableBuilder(
-        valueListenable: monthsBox.listenable(),
-        builder: (context, Box<String> box, _) {
-          final months = box.values.toList();
-          months.sort((a, b) => b.compareTo(a)); // newest first
+        valueListenable: expenseList.listenable(),
+        builder: (context, expenseListValue, child) {
+          return ValueListenableBuilder(
+            valueListenable: monthsBox.listenable(),
+            builder: (context, Box<String> box, _) {
+              final months = box.values.toList();
+              months.sort((a, b) => b.compareTo(a)); // newest first
+          
+              if (months.isEmpty) {
+                return const Center(child: Text('No months yet'));
+              }
+          
+              return ListView(
+                padding: const EdgeInsets.all(12),
+                children: [
+                  
+          
+                  // 🔹 Month List with Cards
+                  ...months.map((key) {
+                     List<Expense> expenseList =
+                         expenseListValue
+                              .values
+                              .map((e) => e)
+                              .toList();
+                      double categoryTotal = 0;
 
-          if (months.isEmpty) {
-            return const Center(child: Text('No months yet'));
-          }
-
-          return ListView.separated(
-            itemCount: months.length,
-            separatorBuilder: (_, __) => const Divider(height: 0),
-            itemBuilder: (context, index) {
-              final key = months[index];
-              return ListTile(
-                title: Text(monthLabelFromKey(key)),
-                trailing: const Icon(Icons.chevron_right),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (_) => MonthGroupScreen(monthKey:key ,),
-                    ),
-                  );
-                },
+                      for (Expense ct in expenseList) {
+                        if (ct.monthKey == key ) {
+                          categoryTotal += ct.amount;
+                        }
+                      }
+                    return Card(
+                      margin: const EdgeInsets.only(bottom: 12),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(16)),
+                      elevation: 2,
+                      child: ListTile(
+                        contentPadding:
+                            const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+                        title: Text(
+                          monthLabelFromKey(key),
+                          style: const TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.w600),
+                        ),
+                        subtitle:  Text("Total: ₹ ${categoryTotal.toStringAsFixed(2)}"), // TODO: fetch from DB
+                        trailing: const Icon(Icons.arrow_forward_ios, size: 18),
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (_) => MonthGroupScreen(monthKey: key),
+                            ),
+                          );
+                        },
+                      ),
+                    );
+                  }),
+                ],
               );
             },
           );
-        },
+        }
       ),
-      // floatingActionButton: FloatingActionButton.extended(
-      //   onPressed: () {
-      //     showModalBottomSheet(
-      //       context: context,
-      //       isScrollControlled: true,
-      //       builder: (_) => const AddExpenseSheet(),
-      //     );
-      //   },
-      //   label: const Text('Add Expense'),
-      //   icon: const Icon(Icons.add),
-      // ),
     );
   }
 }
+
+
+// class MonthScreen extends StatelessWidget {
+//   const MonthScreen({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final monthsBox = Hive.box<String>(kBoxMonths);
+
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Months'),
+//         actions: [
+//           IconButton(
+//             tooltip: 'Add current month',
+//             icon: const Icon(Icons.add_box_outlined),
+//             onPressed: () async {
+//               await ensureCurrentMonthExists();
+//             },
+//           ),
+//           IconButton(
+//             tooltip: 'Settings',
+//             icon: const Icon(Icons.settings),
+//             onPressed: () =>Navigator.push(
+//                 context,
+//                 MaterialPageRoute(builder: (_) => const CategoriesScreen()),
+//               ),
+//           ),
+//         ],
+//       ),
+//       body: ValueListenableBuilder(
+//         valueListenable: monthsBox.listenable(),
+//         builder: (context, Box<String> box, _) {
+//           final months = box.values.toList();
+//           months.sort((a, b) => b.compareTo(a)); // newest first
+
+//           if (months.isEmpty) {
+//             return const Center(child: Text('No months yet'));
+//           }
+
+//           return ListView.separated(
+//             itemCount: months.length,
+//             separatorBuilder: (_, __) => const Divider(height: 0),
+//             itemBuilder: (context, index) {
+//               final key = months[index];
+//               return ListTile(
+//                 title: Text(monthLabelFromKey(key)),
+//                 trailing: const Icon(Icons.chevron_right),
+//                 onTap: () {
+//                   Navigator.push(
+//                     context,
+//                     MaterialPageRoute(
+//                       builder: (_) => MonthGroupScreen(monthKey:key ,),
+//                     ),
+//                   );
+//                 },
+//               );
+//             },
+//           );
+//         },
+//       ),
+//       // floatingActionButton: FloatingActionButton.extended(
+//       //   onPressed: () {
+//       //     showModalBottomSheet(
+//       //       context: context,
+//       //       isScrollControlled: true,
+//       //       builder: (_) => const AddExpenseSheet(),
+//       //     );
+//       //   },
+//       //   label: const Text('Add Expense'),
+//       //   icon: const Icon(Icons.add),
+//       // ),
+//     );
+//   }
+// }
